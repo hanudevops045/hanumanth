09048ac08b25edbbd3436e7a4556739585711798
hanudevops045 <103065206+hanudevops045@users.noreply.github.com>
2022-10-21 16:12:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ region= "us-west-1"
 
 resource "aws_instance" "myec2" { 
 ami= "ami-017c001a88dd93847" 
-instance_type= "t2.nano"
+instance_type= "t2.micro"
  tags= { 
 Name= "hanumanth-server" }
  }
